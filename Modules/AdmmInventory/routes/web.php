32d0b218de4d252e90a2574426f95513e1f21bea
tadiwamzenda <mzendatp@gmail.com>
2026-07-28 10:29:26 +0200

    refactor(admm): remove Reports/SIM Cards/GPS Devices/Accessories from sidebar, move Audit Log to System section with dedicated route

--- Modules/AdmmInventory/routes/web.php
+++ Modules/AdmmInventory/routes/web.php
@@ -2,10 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\AdmmInventory\Http\Controllers\AdmmInventoryController;
-use Modules\AdmmInventory\Http\Controllers\SimCardController;
 use Modules\AdmmInventory\Http\Controllers\ClientController;
-use Modules\AdmmInventory\Http\Controllers\GpsDeviceController;
-use Modules\AdmmInventory\Http\Controllers\AccessoryController;
+use Modules\AdmmReports\Http\Controllers\ReportController as AdmmReportController;
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::resource('admminventories', AdmmInventoryController::class)->names('admminventory');
@@ -13,29 +11,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 
 Route::middleware(['auth'])->prefix('admin/admm')->name('admin.admm.')->group(function () {
-     // SIM Cards
-    Route::prefix('sim-cards')->name('sim-cards.')->group(function () {
-        Route::get('/',          [SimCardController::class, 'index'])->name('index');
-        Route::get('/create',    [SimCardController::class, 'create'])->name('create');
-        Route::get('/{simCard}/edit', [SimCardController::class, 'edit'])->name('edit');
-    });
+    // Audit Log
+    Route::get('/audit-log', [AdmmReportController::class, 'auditTrail'])->name('audit-log');
     // Clients
     Route::prefix('clients')->name('clients.')->group(function () {
         Route::get('/',               [ClientController::class, 'index'])->name('index');
         Route::get('/create',         [ClientController::class, 'create'])->name('create');
         Route::get('/{client}/edit',  [ClientController::class, 'edit'])->name('edit');
-    });
-    // GPS Devices
-    Route::prefix('gps-devices')->name('gps-devices.')->group(function () {
-        Route::get('/',              [GpsDeviceController::class, 'index'])->name('index');
-        Route::get('/create',        [GpsDeviceController::class, 'create'])->name('create');
-        Route::get('/{device}/edit', [GpsDeviceController::class, 'edit'])->name('edit');
-    });
-    //Accessories
-    Route::prefix('accessories')->name('accessories.')->group(function () {
-        Route::get('/',                   [AccessoryController::class, 'index'])->name('index');
-        Route::get('/create',             [AccessoryController::class, 'create'])->name('create');
-        Route::get('/{accessory}/edit',   [AccessoryController::class, 'edit'])->name('edit');
     });
     //Asset Inventory
     Route::get('/asset-register', function () {
--- Modules/Core/resources/views/layouts/admin.blade.php
+++ Modules/Core/resources/views/layouts/admin.blade.php
@@ -61,11 +61,6 @@
                 Asset Stock
                 </a>
 
-                @php [$active, $cls] = $navLink('', 'Reports', '', 'admin.admm.reports.*'); @endphp
-                <a href="{{ route('admin.admm.reports.index') }}" class="flex items-center gap-3 px-3 py-2 rounded-lg text-sm transition-colors {{ $cls }}">
-                    <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/></svg>
-                    Reports
-                </a>
             </div>
 
             {{-- AFIS --}}
@@ -119,9 +114,8 @@
                     Staff
                 </a>
 
-                @php [$active, $cls] = $navLink('', 'Audit Log', '', 'admin.admm.reports.audit-trail'); @endphp
-                <a href="{{ route('admin.admm.reports.audit-trail') }}" class="flex items-center gap-3 px-3 py-2 rounded-lg text-sm transition-colors {{ $cls }}">
-                    <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"/></svg>
+                @php [$active, $cls] = $navLink('', 'Audit Log', '', 'admin.admm.audit-log'); @endphp
+                <a href="{{ route('admin.admm.audit-log') }}" class="flex items-center gap-3 px-3 py-2 rounded-lg text-sm transition-colors {{ $cls }}">                    <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"/></svg>
                     Audit Log
                 </a>
             </div>
